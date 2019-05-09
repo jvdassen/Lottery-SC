ede4826e7b5fe3bc1be3cfac02eb6b15c8e7db18
Noah Berni <noahberni@dhcp-wlan-uzh-10-12-191-197.uzh.ch>
2019-05-09 12:54:34 +0200

    Change price of ticket

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,6 @@ var Oracle = artifacts.require("Oracle");
 module.exports = async function(deployer) {
   await deployer.deploy(Oracle);
   const maxNum = 10;
-  const price = 1000;
+  const price = web3.utils.toWei('1.0', "ether");
   await deployer.deploy(Lottery,maxNum,price);
 };
