5c2a533042ffef5056102d819e77311d0a57a76b
Jan von der Assen <jan.vda@gmx.ch>
2019-05-20 22:39:53 +0200

    Make hashedsecret generation and revealing automated

--- demo/oneWinner-cli.js
+++ demo/oneWinner-cli.js
@@ -1,19 +1,19 @@
-var Oracle = artifacts.require("./Oracle.sol");
-var Lottery = artifacts.require("./Lottery.sol");
-let catchRevert = require("./Exceptions.js").catchRevert;
+var Oracle = artifacts.require("./Oracle.sol")
+var Lottery = artifacts.require("./Lottery.sol")
+let catchRevert = require("./Exceptions.js").catchRevert
 
-var Player1;
-var Player2;
-var Player3;
-const winningNumber = 0;
+var Player1
+var Player2
+var Player3
+const winningNumber = 0
 
 contract("Lottery and Oracle test", async function(accounts) {
-  Player1 = accounts[0];
-  Player2 = accounts[1];
-  Player3 = accounts[2];
+  Player1 = accounts[0]
+  Player2 = accounts[1]
+  Player3 = accounts[2]
 
   it("Client usage", async function() {
-    var client = new Client();
+    var client = new Client()
 
     var lottery = await client.startSmallLottery()
     var receipt = await client.buyTicket(lottery, 0, Player1)
@@ -22,20 +22,21 @@ contract("Lottery and Oracle test", async function(accounts) {
     var receipt = await client.buyTicket(lottery, 1, Player2)
     var receipt = await client.buyTicket(lottery, 2, Player3)
     await timeout(1000)
-  });
-});
+  })
+})
 
 
 function Client () {
   this.lotteryInstance = null
-  this.oracleInstance = null;
-  this.Oracle = Oracle;
-  this.Lottery = Lottery;
-  this.counter = 0;
-  this.userNumber = null;
-  this.userBalance = 0;
+  this.oracleInstance = null
+  this.Oracle = Oracle
+  this.Lottery = Lottery
+  this.counter = 0
+  this.userNumber = null
+  this.userBalance = 0
 
   this.secrets = [
+	  /*
     {
       hash: "0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6",
       secret: "1",
@@ -51,6 +52,7 @@ function Client () {
       secret: "helo",
       user: null
     }
+    */
   ]
   
 
@@ -60,29 +62,40 @@ function Client () {
     return this.lotteryInstance
   }
 
+  Client.prototype.generateHashedSecret = async function (input) {
+    return web3.utils.keccak256(input)
+  }
   Client.prototype.buyTicket = async function (lotteryInstance, number, player) {
+    var secret = "" + Math.random * 10000000000000
+
+
+    var hashedSecret = await this.generateHashedSecret(secret)
+    this.secrets.push({
+      hash: hashedSecret,
+      secret: secret,
+      user: player
+    })
+
     if(this.counter == 0) {
       this.userNumber = number
       this.userBalance = await web3.utils.fromWei(await web3.eth.getBalance(player))
     }
-    this.secrets[this.counter].user = player
-    await this.lotteryInstance.buyTicket(number, this.secrets[this.counter].hash, {from: player, value: web3.utils.toWei('4.0', "ether")})
+    await this.lotteryInstance.buyTicket(number, hashedSecret, { from: player, value: web3.utils.toWei('4.0', "ether") })
     this.counter++
     await timeout(3000)
     await this.checkAutoReveal()
-    return this.counter;
-
+    return this.counter
   }
   Client.prototype.checkAutoReveal = async function () {
     if(this.counter == 3) {
       console.log('INFO: Lottery has enough players.. Starting autoreveal')
       this.secrets.forEach(async (el) => {
-        await this.oracleInstance.reveal(el.secret, {from: el.user});
+        await this.oracleInstance.reveal(el.secret, {from: el.user})
       })
     }
   }
   Client.prototype.informAboutUpdates = function () {
-    var client = this;
+    var client = this
     this.oracleInstance.LogCommit(function (err, response) {
        console.log('INFO: Someone else just joined your lottery!')
     })
@@ -101,5 +114,5 @@ function Client () {
 }
 
 function timeout(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms))
 }
